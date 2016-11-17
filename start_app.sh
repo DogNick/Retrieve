13dbd327a25b7e13a806ffd38cc09ceeea8b902b
Nick <lihangyu@sogou-inc.com>
2016-11-17 12:08:03 +0800

    uwsgi server added

--- start_app.sh
+++ start_app.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-supervisord -c config/retrieve_supervisor.conf
-cat /dev/null > /search/odin/offline/retrieve-ranker/logs/retrieve_supervisor_err.log
-tail -f /search/odin/offline/retrieve-ranker/logs/retrieve_supervisor_err.log
+sh stop.sh
+nohup uwsgi --thunder-lock conf/uwsgiconfig.ini &
+#tail -f logs/retrieve-ranker.std.log
+
+#supervisord -c config/retrieve_supervisor.conf
+#cat /dev/null > /search/odin/offline/retrieve-ranker/logs/retrieve_supervisor_err.log
+#tail -f /search/odin/offline/retrieve-ranker/logs/retrieve_supervisor_err.log
 
 #pid=`ps aux | grep "python -u app.py" | grep -v "grep" | awk '{print $2}'`
 #kill -9 $pid

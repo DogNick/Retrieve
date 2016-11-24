028b71882469d885261871ea9c20b23014b70115
Nick <lihangyu@sogou-inc.com>
2016-11-24 17:24:45 +0800

    new added

---
+++ requirements.sh
@@ -0,0 +1,5 @@
+ pip install flask  -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+ pip install requests -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+ pip install gevent -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+ pip install redis -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+ yum install libevent

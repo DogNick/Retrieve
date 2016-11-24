72cd16106b0b98ff9f191d06655fd2167dd48f9d
Nick <lihangyu@sogou-inc.com>
2016-11-24 17:17:49 +0800

    add some kernal conf before start

--- start_app.sh
+++ start_app.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #sh stop.sh
+ulimit -n 10000
+echo '128000' > /proc/sys/fs/file-max
+sysctl -w net.core.somaxconn=65535
+sysctl -w net.core.netdev_max_backlog=2500
 nohup uwsgi config/uwsgiconfig.ini &
 #tail -f logs/retrieve-ranker.std.log
 

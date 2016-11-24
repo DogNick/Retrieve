5f11f546114ed92fffd45a3ed815394718172814
Nick <lihangyu@sogou-inc.com>
2016-11-24 17:23:59 +0800

    new scripts added

--- start_app.sh
+++ start_app.sh
@@ -5,13 +5,3 @@ echo '128000' > /proc/sys/fs/file-max
 sysctl -w net.core.somaxconn=65535
 sysctl -w net.core.netdev_max_backlog=2500
 nohup uwsgi config/uwsgiconfig.ini &
-#tail -f logs/retrieve-ranker.std.log
-
-#supervisord -c config/retrieve_supervisor.conf
-#cat /dev/null > /search/odin/offline/retrieve-ranker/logs/retrieve_supervisor_err.log
-#tail -f /search/odin/offline/retrieve-ranker/logs/retrieve_supervisor_err.log
-
-#pid=`ps aux | grep "python -u app.py" | grep -v "grep" | awk '{print $2}'`
-#kill -9 $pid
-#nohup python -u app.py > logs/app.log &
-#tail -f logs/app.log
--- stop.sh
+++ stop.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 uwsgi --stop uwsgi.pid
-#pid=`ps -ef | grep supervisord| grep -v grep| awk '{print $2}'`
-#echo "[superviosrd pid="$pid"]"
-#kill -9 $pid
-#pid=`ps -ef | grep broker_app:app| grep -v grep| head -1 | awk '{print $2}'`
-#echo "[unicorn pid="$pid"]"
-#kill -9 $pid
